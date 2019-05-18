8e35e5f622b6616242bfb66600b5d6a18fb98a49
Tomas <kyg8185@autuni.ac.nz>
2019-05-18 18:41:31 +1200

    updated verious pieces

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <a href = "main.php">Main</a>
 <a href = "createaccount.html">Create Account</a>
 
-<!-- add login.php to action when its ready to do the check -->
+<!-- add 'login.php' to action when its ready to do the check -->
 <form method="post" action="">
 		<p>	<label for="uname">Enter User Name: </label>
 			<input type="text" name="uname" id="uname" /></p>
