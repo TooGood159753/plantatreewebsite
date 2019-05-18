8550b446c32dfe8a947574a34bd55a52bc8b9699
Tomas <kyg8185@autuni.ac.nz>
2019-05-18 16:13:35 +1200

    href updates php test

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
   <a href = "shiplocation.html">Ship Location</a>
   <a href = "profile.html">Profile</a>
   <a href = "logout.html">Logout</a>
+  
+  <?php
+  echo "<p>PHP is working</p>";
+  ?>
 </body>
 </html>
