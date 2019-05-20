2fa6b43d195d46feda081e2adbfd6314a708f65d
ghf4336 <42361147+ghf4336@users.noreply.github.com>
2019-05-20 23:25:47 +1200

    Update store.php

--- store.php
+++ store.php
@@ -4,10 +4,15 @@
 <?php
 	session_start();
 ?>
+<head>
+  <title>Tree store</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 
+</head>
 <body>
-<p>store</p>
-<a href = "purchace.php">Purchace</a>
-<a href = "main.php">Main</a>
+<h1>Plant a Tree </h1>
+<div class="fixed"
+<a href="store.php">Cart</a><br>
+</div>	
 </body>
 </html>
