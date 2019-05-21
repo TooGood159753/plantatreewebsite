8bc6d226a7d0940c7e0af5889289cd91ed8856da
MatthewKevinBall <matthew.kevin.ball@gmail.com>
2019-05-21 21:14:16 +1200

    Update index.php

--- index.php
+++ index.php
@@ -12,21 +12,22 @@
 
 ?>
 <head>
+
 </head>
-<body>
-<h2>login</h2>
+<body bgcolor="#E6E6FA">
 <a href = "main.php">Main</a>
 <a href = "createaccount.php">Create Account</a>
-
-
+<h2><u>Login</u></h2>
 
 <!-- add 'login.php' to action when its ready to do the check -->
 <form method="post" action="login.php">
-		<p>	<label for="uname">Enter User Name: </label>
+		<p>	<label for="uname">Enter User Name: </label><br><br>
 			<input type="text" placeholder="Enter Username" name="uname" id="uname" /></p>
-		<p>	<label for="pword">Enter Password: </label>
+		<p>	<label for="pword">Enter Password: </label><br><br>
 			<input type="password" placeholder="Enter Password" name="pword" id="pword" /></p>
+		<div>
         <p>	<input type="submit" value="Submit" /></p>
+		</div>
 </form>
 
 <?php
