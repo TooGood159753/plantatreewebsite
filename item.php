455263d7b92f04eb399fb69c47b9bf6800a9f928
Tomas <kyg8185@autuni.ac.nz>
2019-05-21 22:52:28 +1200

    item dynamic creation

--- item.php
+++ item.php
@@ -4,4 +4,66 @@
 	
 	echo "<p>",$buycodetree,"</p>";
 	echo "<p>",$buycodegarden,"</p>";
+	
+	$host = "us-cdbr-iron-east-02.cleardb.net";
+	$user = "b74160f7bd6416";
+	$pswd = "3ce40478";
+	$dbnm = "heroku_26350cbea1b7381";
+	
+	$username = $_POST["uname"];
+	$password = $_POST["pword"];
+	
+	$conn = @mysqli_connect($host,
+		$user,
+		$pswd,
+		$dbnm
+	);
+	
+	$querytree = "SELECT * FROM trees";
+	$querygarden = "SELECT * FROM gardening";
+	
+	if(!$conn)
+	{
+		echo "<p>Could Not Retrieve Store Database</p>";
+		exit;
+	}
+	else
+	{
+	
+	$resulttrees = mysqli_query($conn, $querytree);
+	$resultgarden = mysqli_query($conn, $querygarden);
+
+	if(!$resulttrees || !$resultgarden)
+		{
+			echo "Could Not Retrieve Store Information";
+		}
+	else
+		{
+			if(mysqli_num_rows($resulttrees) > 0)
+			{
+				while($row = mysqli_fetch_assoc($resulttrees)) 
+				{
+					if($buycodetree === $row["treename"])
+					{
+						echo "<img src='/",$row["treeimglnk"],"' alt='",$row["treename"],"' height='100' width='100'>";
+						echo "<h2>",$row["treename"],"</h2>";
+						echo "<p>",$row["treedesc"],"</p>";
+					}
+				}
+				
+			}
+			if(mysqli_num_rows($resultgarden) > 0)
+			{
+				while($row = mysqli_fetch_assoc($resultgarden)) 
+				{
+					if($buycodegarden === $row["productname"])
+					{
+						echo "<img src='/",$row["prodimglnk"],"' alt='",$row["productname"],"' height='100' width='100'>";
+						echo "<h2>",$row["productname"],"</h2>";
+						echo "<p>",$row["productdesc"],"</p>";
+					}
+				}
+			}
+		}
+	}
 ?>
