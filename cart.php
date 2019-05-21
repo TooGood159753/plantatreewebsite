d49fe2bb9e7f9a13d6b9035c2cab0ff5deff39c6
Tomas <kyg8185@autuni.ac.nz>
2019-05-22 01:02:06 +1200

    start of cart series of updates

--- cart.php
+++ cart.php
@@ -1,3 +1,12 @@
 <?php
 	
+	session_start();
+	
+	echo $_SESSION["userid"];
+	echo $_SESSION["chosenstore"];
+	echo $_SESSION["item1"];
+	echo $_SESSION["item2"];
+	echo $_SESSION["item3"];
+	echo $_SESSION["item4"];
+	
 ?>
