d2d651c6abb0f460a01fc6d85853ff3c0ebe944d
Tomas <kyg8185@autuni.ac.nz>
2019-05-22 04:30:15 +1200

    Update emptycart.php

--- emptycart.php
+++ emptycart.php
@@ -1,3 +1,12 @@
 <?php
-	//emptycart.php
+	session_start();
+	
+	$_SESSION["chosenstore"] = "";
+	$_SESSION["item1"] = "";
+	$_SESSION["item2"] = "";
+	$_SESSION["item3"] = "";
+	$_SESSION["item4"] = "";
+	
+	header('location:main.php');
+	exit;
 ?>
