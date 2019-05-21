d864588b55224132d9fc08bc423c171e69b47cb9
MatthewKevinBall <matthew.kevin.ball@gmail.com>
2019-05-21 21:29:42 +1200

    Update main.php

--- main.php
+++ main.php
@@ -11,6 +11,7 @@
   <a href = "profile.php">Profile</a>
   <a href = "logout.php">Logout</a>
   
+  <h2>Welcome to Plant a Tree</h2>
   <?php
   echo "<p>PHP is working</p>";
   echo "<p>"+$_SESSION["username"]+"</p>";
