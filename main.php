4b543118deb610d25ebfd673ee1dc6b230509b83
MatthewKevinBall <matthew.kevin.ball@gmail.com>
2019-05-26 20:15:53 +1200

    Update main.php

--- main.php
+++ main.php
@@ -7,16 +7,15 @@
 
 <body bgcolor="#E6E6FA">
 
-
+  <a href = "shiplocation.php">Ship Location</a>
   <a href = "profile.php">Profile</a>
   <a href = "logout.php">Logout</a>
   
   <h2>Welcome to Plant a Tree</h2>
   <?php
   echo "<p>PHP is working</p>";
-  echo "<p> Welcome, ",$_SESSION["username"],"</p>";
-  echo "<p>Choose a Store to begin shopping</p>";
-  <a href = "shiplocation.php">Store Locations</a>
+  echo "<p>",$_SESSION["username"],"</p>";
+  echo "<p>",$_SESSION["userid"],"</p>";
   ?>
 </body>
 </html>
